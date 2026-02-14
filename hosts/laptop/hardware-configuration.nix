4e87c560cb4e844b5e2690e42f9ef3b3ea579ec5
Selim Bucher <selim.bucher@slimmer.ch>
2026-02-14 21:17:58 +0100

    load intel graphics driver early for plymouth

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -9,7 +9,7 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
