8543c86ddc86b74096ceac33c6e8242044da9150
Selim Bucher <selim.bucher@slimmer.ch>
2026-03-30 14:24:22 +0200

    desktop monitors

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,10 @@
   networking.hostName = "desktop";
   deviceConfig = {
     sddmWayland = false;
-    monitor = ["DP-3, 2560x1440@240, 0x0, 1.25, bitdepth, 10"];
+    monitor = [
+      "DP-2, 2560x1440@240, 0x0, 1.25, bitdepth, 10"
+      "DP-3, 2560x1440@240, 2048x0, 1.25, bitdepth, 10"
+      ];
     blur = true;
     extraExecOnce = [
       "steam -silent"
